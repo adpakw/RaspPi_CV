b23238a88b8c0d05aff94c1bb28ed178a45a913f
adpakw <adpakw@gmail.com>
2024-12-02 10:32:20 +0000

    mobilenet

--- app/detect_human.py
+++ app/detect_human.py
@@ -1,26 +1,26 @@
 import cv2
 import sys 
-import RPi.GPIO as GPIO 
+# import RPi.GPIO as GPIO 
 
-LED_PIN = 14 
-BUTTON_PIN = 15
-SERVO_PIN = 18
+# LED_PIN = 14 
+# BUTTON_PIN = 15
+# SERVO_PIN = 18
 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-GPIO.setup(LED_PIN, GPIO.OUT) 
-GPIO.setup(SERVO_PIN, GPIO.OUT)
+# GPIO.setmode(GPIO.BCM) 
+# GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+# GPIO.setup(LED_PIN, GPIO.OUT) 
+# GPIO.setup(SERVO_PIN, GPIO.OUT)
 
-pwm = GPIO.PWM(SERVO_PIN, 50)
-pwm.start(0)
-angle = 0
+# pwm = GPIO.PWM(SERVO_PIN, 50)
+# pwm.start(0)
+# # angle = 0
 
-def SetAngle(angle):
-    duty = abs(angle)/18+2
-    if duty >= 90:
-        duty = 1
-    GPIO.output(SERVO_PIN, True)
-    pwm.ChangeDutyCycle(duty)
+# def SetAngle(angle):
+#     duty = abs(angle)/18+2
+#     if duty >= 90:
+#         duty = 1
+#     GPIO.output(SERVO_PIN, True)
+#     pwm.ChangeDutyCycle(duty)
 
 person_cascade = cv2.CascadeClassifier('/home/raspberry/RaspPi_CV/haarcascade_frontalface_default.xml')
 
@@ -49,19 +49,19 @@ try:
         )
 
         for (x, y, w, h) in persons:
-            GPIO.output(LED_PIN,True) 
-            if x > 160:
-                angle += 20
-            else:
-                angle -= 20
-            SetAngle(angle)
+            # GPIO.output(LED_PIN,True) 
+            # if x > 160:
+            #     angle += 20
+            # else:
+            #     angle -= 20
+            # SetAngle(angle)
             cv2.rectangle(resized_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         cv2.imshow("Human Detection", resized_frame)
-        GPIO.output(LED_PIN,False)
+        # GPIO.output(LED_PIN,False)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            GPIO.cleanup()
+            # GPIO.cleanup()
             break
 finally:
     camera.release()
